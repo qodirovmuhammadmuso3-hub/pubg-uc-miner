ff70666e5a57f4e1e8ed18961c4f75fb45a212c5
User <user@example.com>
2026-04-17 19:24:11 +0500

    Update config with final Vercel URL

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   botUsername: 'your_bot', // <-- BOT USERNAME'INGIZNI SHU YERGA YOZING
   channelLink: 'https://t.me/pubguc_uz', // Obuna bo'lish kerak bo'lgan kanal havolasi
   earnDelay: 3000, // Vazifani bajarishdagi kutish vaqti (ms)
-  apiUrl: 'https://f93eb5a297fa4f77-144-124-192-254.serveousercontent.com', // Serveo Backend API manzili
+  apiUrl: 'https://pubg-uc-miner.vercel.app', // Vercel Backend API manzili
 };
